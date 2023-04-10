763eaad98a49ba0a89876d1d7cbf44472babd771
beattris <beatriceebirim@gmail.com>
2023-04-10 22:22:55 +0100

    passed and used data with event () binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,19 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created';
+  serverName = '';
 
   constructor() {
-    setTimeout(() => { 
+    setTimeout(() => {
       this.allowNewServer = true;
-    }, 2000)
+    }, 2000);
   }
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    // HTMLInputElement is used to inform TS that the type of element is a HTML input element.
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
